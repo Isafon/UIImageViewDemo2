903e6eb337872aa5e97c3cb8834b914b0431a705
Isa Fontana <isfon8@gmail.com>
2022-01-18 13:37:20 -0700

    Finished Project

--- UIImageViewDemo/ViewController.swift
+++ UIImageViewDemo/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    let myImage = UIImage(named: "tianmen mtn")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myImageView.image = myImage
+        
     }
 
-
+    @IBAction func onButtonTap(_ sender: UIButton) {
+        
+        let myImage = UIImage(named: "zhangjiajie-grand-canyon")
+        myImageView.image = myImage
+    }
+    
+    
 }
 
